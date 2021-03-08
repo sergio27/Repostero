55e53196875bc008bbf125f244b1c60cdf16efd9
Sergio <ibarra.sergio@gmail.com>
2021-03-07 18:32:33 -0700

    Despliegue a MongoDB Atlas y Heroku.

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/reposteriaDB", );
+mongoose.connect("mongodb+srv://sergio:Test123@cluster0.zgnw4.mongodb.net/reposteroDB?retryWrites=true&w=majority");
 
 const recetaSchema = new mongoose.Schema ({
   nombre: String,
@@ -40,6 +40,11 @@ app.get("/:receta", function(req, res) {
   });
 });
 
-app.listen(3000, function() {
+let port = process.env.PORT;
+if(port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
   console.log("Server started on port 3000");
 })
